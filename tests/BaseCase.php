c9abea80b0f46f766408097a377caf7a08073f3d
EvgenyGavrilov <e.r.gavrilov@gmail.com>
2016-08-18 17:41:40 +0500

    Client v 0.1 for sima-land api

---
+++ tests/BaseCase.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace simaland\apiClient\tests;
+
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use simaland\apiClient\Client;
+
+class BaseCase extends TestCase
+{
+    /** @var \simaland\apiClient\Client */
+    private $_client;
+
+    /** @var \GuzzleHttp\Handler\MockHandler */
+    private $_mockGuzzleHandler;
+
+    /**
+     * @return \GuzzleHttp\Handler\MockHandler
+     */
+    private function _getMockGuzzleHandler()
+    {
+        if (is_null($this->_mockGuzzleHandler)) {
+            $this->_mockGuzzleHandler = $this->_mockGuzzleHandler = new MockHandler();
+        }
+        return $this->_mockGuzzleHandler;
+    }
+
+    /**
+     * @param mixed $response
+     */
+    protected function setGuzzleHttpResponse($response)
+    {
+        $this->_getMockGuzzleHandler()->append($response);
+    }
+
+    /**
+     * @param array $body
+     * @param int $statusCode
+     */
+    protected function setResponse(array $body, $statusCode = 200)
+    {
+        $body = json_encode($body);
+        $this->setGuzzleHttpResponse(
+            new Response($statusCode, ['Content-Type' => 'application/json'], $body)
+        );
+    }
+
+    /**
+     * @return \simaland\apiClient\Client
+     */
+    protected function getClient()
+    {
+        if (is_null($this->_client)) {
+            $handler = HandlerStack::create($this->_getMockGuzzleHandler());
+            $guzzleClient = new \GuzzleHttp\Client(['handler' => $handler]);
+            $this->_client = new Client();
+            $this->_client->setHttpClient($guzzleClient);
+        }
+        return $this->_client;
+    }
+}
---
+++ tests/ClientTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace simaland\apiClient\tests;
+
+use GuzzleHttp\Psr7\Response;
+use simaland\apiClient\Client;
+
+class ClientTest extends BaseCase
+{
+    public function testCreateClient()
+    {
+        $url = 'http://example.com';
+        $client = new Client(['baseUrl' => $url]);
+        $this->assertEquals($url, $client->baseUrl);
+        $this->assertInstanceOf('\GuzzleHttp\Client', $client->getHttpClient());
+    }
+
+    public function testQuery()
+    {
+        $client = $this->getClient();
+        $body = ['foo' => 'bar'];
+        $this->setResponse($body);
+        $result = $client->get('item');
+        $this->assertEquals($body, $result);
+        $body = 'raw body';
+        $this->setGuzzleHttpResponse(new Response(200, [], $body));
+        $result = $client->query('GET', 'item', ['key' => 'value']);
+        $this->assertEquals($body, $result);
+    }
+}
